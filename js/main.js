341c38c259bc0cc4e615c3d8047d4014ff2f6a03
Takuto Arima <ta910arima@gmail.com>
2020-10-23 13:55:05 +0900

    ハンバーガー機能修正

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 "use strict"
 {
-  const content = queryASelectorAll("accordion_content");
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const overlay = document.querySelector('.accordion_content');
+  const nav = document.getElementById('sp_nav');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  })
+
+  nav.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  })
 
 }
