a74a91a66c5ce7cdf005704a474601c45d98b560
Takuto Arima <ta910arima@gmail.com>
2020-10-26 10:22:52 +0900

    profile修正

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 "use strict"
 {
   const open = document.getElementById('open');
+  const close = document.getElementById('sp_nav');
   const overlay = document.querySelector('.accordion_content');
-  const ul = document.getElementById('sp_nav');
+  const image = document.querySelector('.profile_image');
+  const cb = function(entries, observer) {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        console.log('in view');
+        entry.target.classList.add('left');
+        observer.unobserve(entry.target);
+      } else
+        console.log('out view');
+    });
+  }
+  const io = new IntersectionObserver(cb);
+  io.observe(image);
 
+  /* ハンバーガー オープン*/
   open.addEventListener('click', () => {
     overlay.classList.toggle('show');
     open.classList.toggle('active');
   });
 
-  ul.addEventListener('click', () => {
+  /* ハンバーガー クローズ*/
+  close.addEventListener('click', () => {
     overlay.classList.remove('show');
     open.classList.remove('active');
   });
+
+  /*自己紹介スクロール*/
 
 }
