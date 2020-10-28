ff836a988e936fac93a05261365a3e979798c90a
Takuto Arima <ta910arima@gmail.com>
2020-10-28 14:29:12 +0900

    work修正

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
 "use strict"
 {
-  const open = document.getElementById('open');
-  const close = document.getElementById('sp_nav');
-  const overlay = document.querySelector('.accordion_content');
-  const image = document.querySelector('.profile_image');
-  const cb = function(entries, observer) {
-    entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        console.log('in view');
-        entry.target.classList.add('left');
-        observer.unobserve(entry.target);
-      } else
-        console.log('out view');
-    });
-  }
-  const io = new IntersectionObserver(cb);
-  io.observe(image);
+  const open = document.getElementById('open');                   /* ハンバーガー オープン*/
+  const close = document.getElementById('sp_nav');                /* ハンバーガー クローズ*/
+  const overlay = document.querySelector('.accordion_content');   /* ハンバーガー メニュー*/
+  const windowWidth = document.documentElement.clientWidth;       /* 画面幅 */
+  const sp = 768;                                                 /* レスポンシブ画面幅 */
+  const image = document.querySelector('.profile_image');         /*自己紹介 写真*/
+  const profile = document.querySelector('.profile_text');        /*自己紹介 テキスト*/
+  const works = document.querySelectorAll(".work_content");       /* 仕事 */
 
   /* ハンバーガー オープン*/
   open.addEventListener('click', () => {
@@ -29,6 +21,51 @@
     open.classList.remove('active');
   });
 
-  /*自己紹介スクロール*/
+  window.addEventListener('resize', function(){
+    if (windowWidth > sp ) {
+      overlay.classList.remove('show');
+      open.classList.remove('active');
+    }
+  });
 
+  /*自己紹介スクロール*/
+  const about_left = function(entries, observer) {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add('left');
+        observer.unobserve(entry.target);
+      }
+    });
+  }
+
+  const about_right = function(entries, observer) {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add('right');
+        observer.unobserve(entry.target);
+      }
+    });
+  }
+
+  const iol = new IntersectionObserver(about_left);
+  const ior = new IntersectionObserver(about_right);
+  iol.observe(image);
+  ior.observe(profile);
+
+  /* 仕事 */
+  works.forEach(wk => {
+    const text = wk.querySelector('.work_text');
+    const img = wk.querySelector('.work_img');
+    const work_cb = function(entries) {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add('work_show');
+        }
+      });
+    }
+
+    const work = new IntersectionObserver(work_cb);
+    work.observe(text);
+    work.observe(img);
+  });
 }
